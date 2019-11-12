fd41cb16ba33d501dced9dd1b0286284fe775234
Mathis Kretz <mathis.kretz@gmail.com>
2019-11-12 22:10:24 +0100

    Add source files

---
+++ README.md
@@ -0,0 +1,27 @@
+# notification-mailer-k8s
+
+Docker / kubernetes version of notification-mailer
+
+## Create a new version
+
+1. Build a new docker image
+
+```
+docker build --no-cache -t mathiskretz/notification-mailer .
+```
+
+2. Push the docker image to dockerhub
+
+```
+docker push mathiskretz/notification-mailer:latest
+```
+
+## Requirements to run
+
+1. Assumes that there exists a RabbitMQ service named `notification-mq`
+2. Assumes that the environment variable `MAIL_URL` is containing a valid SMTP URI
+3. Assumes that the environment variable `TO_ADDRESS` is containing the email address to which all notification mails are sent
+
+## Deployment
+
+Use [these YAML files](https://github.com/bespinian/notification-k8s).
---
+++ index.js
@@ -0,0 +1,42 @@
+var rabbitmqUri = "amqp://notification-mq";
+var smtpUri = process.env.MAIL_URL;
+var amqp = require("amqplib/callback_api");
+var nodemailer = require("nodemailer");
+var transporter = nodemailer.createTransport(smtpUri);
+
+function sendMail(subject, message) {
+  var sendingAddress = process.env.TO_ADDRESS;
+  var mailOptions = {
+    to: sendingAddress,
+    subject: subject,
+    text: message
+  };
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message %s sent: %s", info.messageId, info.response);
+  });
+}
+
+amqp.connect(rabbitmqUri, function(err, conn) {
+  conn.createChannel(
+    function(err, ch) {
+      var queue = "notifications";
+      ch.assertQueue(queue, { durable: false });
+      console.log(
+        " [*] Waiting for messages in %s. To exit press CTRL+C",
+        queue
+      );
+      ch.consume(queue, function(msg) {
+        var message = JSON.parse(msg.content);
+        console.log(message);
+        sendMail(
+          "Notification!",
+          "Hi, here's a new notification: " + message.text
+        );
+      });
+    },
+    { noAck: true }
+  );
+});
