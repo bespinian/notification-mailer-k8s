de9fb0a9db8e53c609f530248b43f556ad57ccd4
Mathis Kretz <mathis@bespinian.io>
2021-11-15 23:27:54 +0100

    Add simulation mode for logging without sending real mails

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var rabbitmqUri = process.env.MQ_URL || "amqp://notification-mq";
 var smtpUri = process.env.MAIL_URL;
+var simulate = process.env.SIMULATE === "true" || false;
 var amqp = require("amqplib/callback_api");
 var nodemailer = require("nodemailer");
 var transporter = nodemailer.createTransport(smtpUri);
@@ -9,26 +10,35 @@ function sendMail(subject, message) {
   var mailOptions = {
     to: sendingAddress,
     subject: subject,
-    text: message
+    text: message,
   };
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      return console.log(error);
-    }
-    console.log("Message %s sent: %s", info.messageId, info.response);
-  });
+  if (simulate) {
+    console.log(
+      "Simulated sending of message. Subject: %s, Text: %s, Address: %s",
+      mailOptions.subject,
+      mailOptions.text,
+      mailOptions.to
+    );
+  } else {
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("Message %s sent: %s", info.messageId, info.response);
+    });
+  }
 }
 
-amqp.connect(rabbitmqUri, function(err, conn) {
+amqp.connect(rabbitmqUri, function (err, conn) {
   conn.createChannel(
-    function(err, ch) {
+    function (err, ch) {
       var queue = "notifications";
       ch.assertQueue(queue, { durable: false });
       console.log(
         " [*] Waiting for messages in %s. To exit press CTRL+C",
         queue
       );
-      ch.consume(queue, function(msg) {
+      ch.consume(queue, function (msg) {
         var message = JSON.parse(msg.content);
         console.log(message);
         sendMail(
